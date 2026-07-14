1eb3c9bb02b98ca1526142467447b1e5de0186bd
Shaid Azmin <excel.azmin@gmail.com>
2026-07-14 07:10:57 +0000

    add fixtures and new doctype

---
+++ excel_erpnext/excel_erpnext/doctype/live_project_dashboard/live_project_dashboard.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Castlecraft Ecommerce Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Live Project Dashboard', {
+	// refresh: function(frm) {
+
+	// }
+});
