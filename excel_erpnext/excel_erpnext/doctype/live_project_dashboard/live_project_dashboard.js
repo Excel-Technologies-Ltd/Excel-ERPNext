046481a3cc6f7f9b24d5314f8d05441e3e1628e9
Shaid Azmin <excel.azmin@gmail.com>
2026-07-21 12:09:02 +0000

    complete project calculation

--- excel_erpnext/excel_erpnext/doctype/live_project_dashboard/live_project_dashboard.js
+++ excel_erpnext/excel_erpnext/doctype/live_project_dashboard/live_project_dashboard.js
@@ -2,7 +2,39 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Live Project Dashboard', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		calculate_totals(frm);
+	}
 });
+
+function calculate_totals(frm) {
+	let total_running_cost = flt(frm.doc.others_cost)
+		+ flt(frm.doc.material_cost)
+		+ flt(frm.doc.sub_contractor_cost)
+		+ flt(frm.doc.labor_cost)
+		+ flt(frm.doc.team_cost);
+
+	let actual_cost = total_running_cost;
+
+	let profitloss_amount = flt(frm.doc.total_wo_value) - actual_cost;
+
+	let profit_percentage = flt(frm.doc.total_wo_value)
+		? (profitloss_amount / flt(frm.doc.total_wo_value)) * 100
+		: 0;
+
+	let remaining_budget = flt(frm.doc.total_budget_cost) - actual_cost;
+
+	let remaining_iou = actual_cost - (flt(frm.doc.approved_iou) + flt(frm.doc.pending_iou));
+
+	let pending_voucher = flt(frm.doc.approved_iou) - flt(frm.doc.iou_voucher);
+
+	frm.set_value({
+		total_running_cost: total_running_cost,
+		actual_cost: actual_cost,
+		profitloss_amount: profitloss_amount,
+		profit_percentage: profit_percentage,
+		remaining_budget: remaining_budget,
+		remaining_iou: remaining_iou,
+		pending_voucher: pending_voucher,
+	});
+}
